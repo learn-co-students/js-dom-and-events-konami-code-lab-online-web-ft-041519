a7a41a2405d68f4f94635798c85d0117d060fa5b
michellemoon236 <michellemoon236@gmail.com>
2019-07-28 20:26:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.querySelector('body');
+
+let index = 0;
+
+body.addEventListener('keydown', onKeyDownHandler)
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length - 1) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
 function init() {
-  // Write your JavaScript code inside the init() function
 
 }
